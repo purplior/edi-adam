e66a7f8b73390b28dbd4ec7248a62ca9f8bcabed
devphilip21 <philip21.dev@gmail.com>
2024-09-15 00:36:46 +0900

    feat: 로그 메세지 추가

--- domain/exception/common_exception.go
+++ domain/exception/common_exception.go
@@ -3,7 +3,7 @@ package exception
 import "errors"
 
 var (
-	ErrBadRequest    = errors.New("bad request")
-	ErrUnauthorized  = errors.New("unauthorized")
-	ErrNotAcceptable = errors.New("not acceptable")
+	ErrBadRequest    = errors.New("잘못된 요청이에요")
+	ErrUnauthorized  = errors.New("인증에 실패 했어요")
+	ErrNotAcceptable = errors.New("받아들일 수 없는 요청이에요")
 )
